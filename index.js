662e1331d3c584f189d2391f83b840e4e3ccf245
amaurypavao <amaury.pavao@vtex.com.br>
2024-05-31 15:23:01 -0300

    incluido o método PATCH

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const VTEX_APP_KEY = process.env.VTEX_APP_KEY;
 const VTEX_APP_TOKEN = process.env.VTEX_APP_TOKEN;
 
 async function fetchMasterData(entity, documentId) {
-  const url = `https://${VTEX_ACCOUNT_NAME}.${VTEX_ENVIRONMENT}.com.br/api/dataentities/${entity}/documents/${documentId}`;
+  const url = `https://${VTEX_ACCOUNT_NAME}.${VTEX_ENVIRONMENT}.com.br/api/dataentities/${entity}/documents/${documentId}?_fields=_all`;
   const headers = {
     'Content-Type': 'application/json',
     'X-VTEX-API-AppKey': VTEX_APP_KEY,
@@ -22,7 +22,18 @@ async function fetchMasterData(entity, documentId) {
   return response.data;
 }
 
-app.post('/api/vtex-master-data', async (req, res) => {
+async function updateMasterData(entity, documentId, updateData) {
+  const url = `https://${VTEX_ACCOUNT_NAME}.${VTEX_ENVIRONMENT}.com.br/api/dataentities/${entity}/documents/${documentId}`;
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-VTEX-API-AppKey': VTEX_APP_KEY,
+    'X-VTEX-API-AppToken': VTEX_APP_TOKEN,
+  };
+  const response = await axios.patch(url, updateData, { headers });
+  return response.data;
+}
+
+app.get('/api/vtex-master-data', async (req, res) => {
   try {
     const { entity, documentId } = req.body;
     if (!entity || !documentId) {
@@ -33,6 +44,20 @@ app.post('/api/vtex-master-data', async (req, res) => {
   } catch (error) {
     console.error('Error fetching data from VTEX Master Data:', error);
     res.status(500).send('Error fetching data from VTEX Master Data');
+  }
+});
+
+app.patch('/api/vtex-master-data', async (req, res) => {
+  try {
+    const { entity, documentId, updateData } = req.body;
+    if (!entity || !documentId || !updateData) {
+      return res.status(400).send('Entity, Document ID, and Update Data are required');
+    }
+    const updatedData = await updateMasterData(entity, documentId, updateData);
+    res.json(updatedData);
+  } catch (error) {
+    console.error('Error updating data in VTEX Master Data:', error);
+    res.status(500).send('Error updating data in VTEX Master Data');
   }
 });
 
